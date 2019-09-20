58704a6e569aac00513e0358702e37cae8a36bef
mariogabrielcampos@gmail.com <mariogabrielcampos@gmail.com>
2019-09-20 23:49:06 +0000

    minor changes

--- src/morse_to_ascii.c
+++ src/morse_to_ascii.c
@@ -2,12 +2,14 @@
 #include "fancy_terminal.h"
 
 void convert_morse_to_ascii(const char *sentence) {
-    static const char tree[] = "ETIANMSURWDKGOHVF-L-PJBXCYZQ- ";
+    const char tree[] = "ETIANMSURWDKGOHVF-L-PJBXCYZQ- ";
     int i;
+
     printf("Assuming Morse => ASCII conversion\n");
     printf("[Original (Morse)]: %s\n", sentence);
     set_cursor_cyan();
     printf("[ASCII]: ");
+
     while ((*sentence) != '\0') {
         i = 0;
         while ((*sentence) != ' ') {
