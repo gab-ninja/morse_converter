3c2e62fe604ed62606e582e4ec3d27c87bbc419e
mariogabrielcampos@gmail.com <mariogabrielcampos@gmail.com>
2019-09-21 16:42:05 +0000

    added a to m and improved m to a

--- src/ascii_to_morse.c
+++ src/ascii_to_morse.c
@@ -1,9 +1,36 @@
+#include <ctype.h>
 #include <stdio.h>
 #include "fancy_terminal.h"
 
-void convert_ascii_to_morse(const char snectence[]) {
+void convert_ascii_to_morse(char *sentence) {
+    const char *morse_codes[27] = {
+        ".-",   "-...", "-.-.", "-..",  ".",   "..-.", "--.",  "....", "..",
+        ".---", "-.-",  ".-..", "--",   "-.",  "---",  ".--.", "--.-", ".-.",
+        "...",  "-",    "..-",  "...-", ".--", "-..-", "-.--", "--..", "----"};
+
+    int invalid_characters = 0;
+
     printf("Assuming ASCII => Morse conversion\n");
-    printf("[Original (ASCII)]: %s\n", snectence);
+    printf("[Original (ASCII)]: %s\n", sentence);
     set_cursor_cyan();
-    printf("[Morse]: %s\n", snectence);
+    printf("[Morse]: ", morse_codes[1]);
+
+    while ((*sentence) != '\0') {
+        if ((*sentence) == 32) {
+            printf("%s ", morse_codes[26]);
+        } else {
+            (*sentence) = toupper((*sentence));
+            if ((*sentence) >= 65 && (*sentence) <= 122) {
+                printf("%s ", morse_codes[(*sentence) - 65]);
+            } else {
+                invalid_characters = 1;
+            }
+        }
+        sentence++;
+    }
+    printf("\n");
+    if (invalid_characters) {
+        set_cursor_red();
+        printf("[Warning]: some characters were ignored\n");
+    }
 }
--- src/morse_to_ascii.c
+++ src/morse_to_ascii.c
@@ -3,28 +3,24 @@
 
 void convert_morse_to_ascii(const char *sentence) {
     const char tree[] = "ETIANMSURWDKGOHVF-L-PJBXCYZQ- ";
-    int i;
+    int i = 0;
 
     printf("Assuming Morse => ASCII conversion\n");
     printf("[Original (Morse)]: %s\n", sentence);
     set_cursor_cyan();
     printf("[ASCII]: ");
 
-    while ((*sentence) != '\0') {
-        i = 0;
-        while ((*sentence) != ' ') {
-            if ((*sentence) == '\0') {
-                printf("%c\n", tree[--i]);
-                return;
-            }
-            if ((*sentence) == '.') {
-                i = 2 * i + 1;
-            } else {
-                i = 2 * i + 2;
-            }
-            sentence++;
+    for (; *sentence; ++sentence) {
+        if ((*sentence) == ' ') {
+            printf("%c", tree[--i]);
+            i = 0;
+            continue;
         }
-        printf("%c", tree[--i]);
-        sentence++;
+        if ((*sentence) == '.') {
+            i = 2 * i + 1;
+        } else {
+            i = 2 * i + 2;
+        }
     }
+    printf("%c\n", tree[--i]);
 }
